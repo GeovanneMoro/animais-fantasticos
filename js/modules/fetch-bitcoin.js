5dc602e49f70beb1541d9dfebb9d30f0e5326c95
Geovanne Moro <geovannemoro@hotmail.com>
2020-08-22 13:01:08 -0300

    fetch bitcoin refatorado

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,15 +1,22 @@
-export default function initBitcoinFetch() {
-  async function fetchBitcoin(url) {
-    try {
-      const fetchResponse = await fetch(url);
-      const fetchJSON = await fetchResponse.json();
-      const btcPreco = document.querySelector(".btc-preco");
+export default function fetchBitcoin(url, target) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((bitcoin) => {
+      const btcPreco = document.querySelector(target);
+      btcPreco.innerText = (1000 / bitcoin.BRL.sell).toFixed(4);
+    })
+    .catch((erro) => console.log(erro));
+  // async function fetchBitcoin(url) {
+  //   try {
+  //     const fetchResponse = await fetch(url);
+  //     const fetchJSON = await fetchResponse.json();
+  //     const btcPreco = document.querySelector(".btc-preco");
 
-      btcPreco.innerText = (10000 / fetchJSON.BRL.sell).toFixed(4);
-    } catch (erro) {
-      console.log(erro);
-    }
-  }
+  //     btcPreco.innerText = (10000 / fetchJSON.BRL.sell).toFixed(4);
+  //   } catch (erro) {
+  //     console.log(erro);
+  //   }
+  // }
 
-  fetchBitcoin("https://blockchain.info/ticker");
+  // fetchBitcoin("https://blockchain.info/ticker");
 }
